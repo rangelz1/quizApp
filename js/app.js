cafa79e552f7c1dad99f7695d57433e1f66451bd
rangelz1 <rangelz187@gmail.com>
2017-04-25 20:25:26 -0700

    Quiz choices and guesses are operational and some changes to style where done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,36 @@
 //this function populates the question into the quiz
 function populate() {
 	if(quiz.isEnded()) {
-		//showScores();
+		showScores();
 	}
 	else {
 		//show question
 		var element = document.getElementById("question");
 		element.innerHTML = quiz.getQuestionIndex().text;
+
+		// show choices
+		var choices = quiz.getQuestionIndex().choices;
+		for(var i = 0; i< choices.length; i++) {
+			var element = document.getElementById("choice" + i);
+			element.innerHTML = choices[i];
+			guess("btn" + i, choices[i]);
+		}
 	}
+};
+
+function guess(id, guess){
+	var button = document.getElementById(id);
+	button.onclick = function() {
+		quiz.guess(guess);
+		populate();
+	}
+}
+
+function showScores() {
+	var gameOverHtml = "<h1>Result</h1>";
+		gameOverHtml = "<h2 id='score'> Your score: " + quiz.score + "</h2>";
+	var element = document.getElementById("quiz");
+	element.innerHTML = gameOverHtml;
 }
 
 //This variables recive the questions from 
